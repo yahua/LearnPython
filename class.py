22eb1413e7786511d8c43f4e5ecc2504adf93b4c
yahua <wangsw@wangswdeMac-mini.local>
2015-03-09 19:11:25 +0800

    类额私有属性

--- class.py
+++ class.py
@@ -2,10 +2,10 @@ class Student(object):
 	"""docstring for Student"""
 	def __init__(self, name, score):
 		super(Student, self).__init__()
-		self.name = name
-		self.score = score
+		self.__name = name   	#__ define private property
+		self.__score = score
 	def print_score(self):
-			print '%s: %s' % (self.name, self.score)	
+			print '%s: %s' % (self.__name, self.__score)	
 bart = Student('Bart Simpson', 59)		
 lisa = Student('Lisa Simpson', 87)	
 bart.print_score()
